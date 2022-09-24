1896cbb85214d8ba83c4ef30a23396c6f026aec8
Alexander Baklushin <din.myrk@gmail.com>
2022-09-25 02:02:44 +0300

    1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 8, -4, 6, 7));
+        List<String> words = new ArrayList<>(List.of("Book", "even", "Book", "string", "Sun", "notebook", "Sun"));
+        System.out.println(outputsOddNumbers(nums));
+        System.out.println(outputsEvenNumbers(nums));
+        System.out.println(outputsUniqueWords(words));
+        System.out.println(outputsNumberOfTakes(words));
+
     }
+
+    public static List<Integer> outputsOddNumbers(List<Integer> integerList) {
+        List<Integer> oddNumbers = new ArrayList<>();
+        for (Integer integer : integerList) {
+            if (integer % 2 == 1) {
+                oddNumbers.add(integer);
+            }
+        }
+        return oddNumbers;
+    }
+
+    public static List<Integer> outputsEvenNumbers(List<Integer> integerList) {
+        Set<Integer> evenNumbersSet = new HashSet<>();
+        for (Integer integer : integerList) {
+            if (integer % 2 == 0) {
+                evenNumbersSet.add(integer);
+            }
+        }
+        List<Integer> evenNumbersList = new ArrayList<>(evenNumbersSet);
+        Collections.sort(evenNumbersList);
+        return evenNumbersList;
+    }
+
+    public static Set<String> outputsUniqueWords(List<String> words) {
+        return new HashSet<>(words);
+    }
+
+    public static HashMap<String, Integer> outputsNumberOfTakes(List<String> words) {
+        HashMap<String, Integer> numbersOfTakes = new HashMap<>();
+        for (String word : words) {
+            if (numbersOfTakes.containsKey(word)) {
+                int take = numbersOfTakes.get(word);
+                numbersOfTakes.put(word, take + 1);
+            } else {
+                numbersOfTakes.put(word, 1);
+            }
+        }
+        return numbersOfTakes;
+    }
+
+
 }
